115683614a4bf6048b650699197d952e18a15954
Dinah Marie De Paz Animos <126230640+maridanne@users.noreply.github.com>
2024-03-07 15:11:02 +0800

    layout-using-component

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,16 @@
+<x-app-web-layout>
+    <x-slot:title>
+        DOT Reg 8 TAMSTAR
+    </x-slot>
+    <div class="py-5">
+        <div class="container">
+            <h4>Välkommen på Index Page</h4>
+        </div>
+    </div>
+
+    <x-slot name="scripts">
+        <script>
+            alert('This is the script area.');
+        </script>
+    </x-slot>
+</x-app-web-layout>
